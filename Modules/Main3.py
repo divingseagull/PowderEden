d4077a1207d05736137685fe68287ac96be36dbe
FilePile2856 <88542418+FilePile2856@users.noreply.github.com>
2021-08-09 22:57:04 +0900

    Update Main3.py

--- Modules/Main3.py
+++ Modules/Main3.py
@@ -6,8 +6,10 @@ import os
 import json
 modules = []
 app = commands.Bot(command_prefix="!")
-
-token = "afdsssssssssssssssssssss"
+yt = 0
+xm = 0
+ym = 0
+token = "ㅁㄴㅇㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ"
 a = [[0 for i in range(20)] for i in range(20)]
 
 @app.command(name='배치')
@@ -18,21 +20,35 @@ async def roll(ctx, Unit, xt, yt):
     x = int(xt)
     y = int(yt)
     a[x-1][y-1] = 1
-    for i in range(0,10):
-        for j in range(0,10):
+    for i in range(0,20):
+        for j in range(0,20):
             print(a[j][i],end= ' ')
         print()
-    JSONUtils.write("Data/Map.json", object=a)
+    JSONUtils.write("discord/Data/Map.json", object=a)
 @app.command(name='생성')
 async def roll(ctx, Unit, Count):
     await ctx.send(f'{Unit}이(가) {Count}개 생성되었습니다')
 @app.command(name='이동')
-async def roll(ctx, Unit, x, y):
-    await ctx.send(f'{x}x {y}y로 {Unit}이(가) 이동되었습니다')
+async def roll(ctx, Unit,xt,yt, xm, ym):
+    await ctx.send(f'{xm}x {ym}y로 {Unit}이(가) 이동되었습니다')
 
-@app.command(name='생성')
-async def roll(ctx, Unit, Count):
-    await ctx.send(f'{Unit}이(가) {Count}개 생성되었습니다')
+    print(xm,ym)
+    #print(a)
+    xt = int(xt)
+    yt = int(yt)
+    x = int(xm)
+    y = int(ym)
+    a[xt-1][yt-1] = 0
+    a[x-1][y-1] = 1
+    for i in range(0,20):
+        for j in range(0,20):
+            print(a[j][i],end= ' ')
+        print()
+
+@app.command(name='맵')
+async def roll(ctx):
+    await ctx.send(f'{a}')
+
 
 @app.event
 async def on_ready():
