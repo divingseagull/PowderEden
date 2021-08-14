8ed80285566a72d8bf51e68b3a1138215bbe040f
KFromage <projw1024@naver.com>
2021-08-14 18:49:32 +0900

    made Image.py

---
+++ Modules/Image.py
@@ -0,0 +1,28 @@
+from PIL import Image
+
+
+def make_map_image(size, background, pixels: dict):
+    """
+    args 인자에서 색상, 위치를 받아서 이미지를 생성함
+
+    size = (x, y)
+
+    background = (R, G, B)
+
+    pixels = {
+        (R, G, B): [
+            (x, y),
+            ...
+        ],
+        ...
+    }
+    """
+
+    img = Image.new(mode="RGB", size=(size[0], size[1]), color=background)
+
+    for rgb in pixels:
+        for xy in pixels[rgb]:
+            img.putpixel(xy, rgb)
+
+    return img
+
