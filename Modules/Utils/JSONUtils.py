07eba67fae5e65204ee8aa29460309d1746e6c2a
KFromage <projw1024@naver.com>
2021-08-17 18:44:59 +0900

    JSONUtils.py

--- Modules/Utils/JSONUtils.py
+++ Modules/Utils/JSONUtils.py
@@ -5,15 +5,15 @@ from .Errors import *
 
 
 class JSONUtils:
-    def load(filePath: str) -> Union[dict, list]:
+    def load(self, filePath: str) -> Union[dict, list]:
         with open(file=filePath, mode="r") as File:
             return json.load(fp=File)
 
-    def write(filePath: str, object: Union[dict, list], indent: int = 4) -> None:
+    def write(self, filePath: str, object: Union[dict, list], indent: int = 4) -> None:
         with open(file=filePath, mode="w") as File:
             json.dump(fp=File, obj=object, indent=indent)
 
-    def update(filePath: str, object: Union[dict, list], indent: int = 4) -> None:
+    def update(self, filePath: str, object: Union[dict, list], indent: int = 4) -> None:
         with open(file=filePath, mode="r") as File:
             try:
                 tmp: dict = json.load(fp=File)
